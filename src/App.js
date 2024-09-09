48a25712e1844a1932ae6df00e8020a7d61b0200
minorscarf <minorscarf@gmail.com>
2024-09-09 23:39:43 +0900

    ボタンの処理を追加

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick(){
+    if(count === 0)
+      {
+        setCount(count + 1);
+      }
+      else if(count < 100000)
+      {
+        setCount(count * 2);
+      }
+      else
+      {
+        setCount(0);
+      }
+  }
+  
   return (
     <div className = "App">
       <header className='App-header'>
       <img src={logo} className="App-logo" alt="logo" />
       <h1>
-          WelCome to my App
-          <MyButton/>
-          <AboutPage/>
-        </h1>
+        <MyButton count={count} onClick={handleClick}/>
+        <MyButton count={count} onClick={handleClick}/>
+      </h1>
       </header>
     </div>
   );
 }
 
-function MyButton() {
+function MyButton({count, onClick}) {
   return (
-    <button>
-      I'm a button
+    <button onClick={onClick}>
+      Clicked {count} times;
     </button>
   );
 }
