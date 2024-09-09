442fd8289ce5e9fa1ddac129e618654c75c815bf
minorscarf <minorscarf@gmail.com>
2024-09-09 23:08:20 +0900

    ボタン追加したり
    ボタンを追加
    ロゴの色や大きさを変更
    ロゴにアニメーションを付けた

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "App">
+      <header className='App-header'>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+          WelCome to my App
+          <MyButton/>
+          <AboutPage/>
+        </h1>
       </header>
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hello there.<br />How do you do?
+      </p>
+    </>
   );
 }
 
