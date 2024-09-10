64255423ad3de18c4c616c418ebc7c90a3d00e0e
minorscarf <minorscarf@gmail.com>
2024-09-10 12:33:16 +0900

    テキストボックスを作成

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import {useState} from 'react';
+import TextField from '@mui/material/TextField';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,7 @@ function App() {
       {
         setCount(count + 1);
       }
-      else if(count < 100000)
+      else if(count *2  < 100000)
       {
         setCount(count * 2);
       }
@@ -24,10 +25,12 @@ function App() {
     <div className = "App">
       <header className='App-header'>
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>
-        <MyButton count={count} onClick={handleClick}/>
-        <MyButton count={count} onClick={handleClick}/>
-      </h1>
+      <h2>
+        Welcome to my App  
+      </h2>
+      <MyButton count={count} onClick={handleClick}/>
+      <MyButton count={count} onClick={handleClick}/>
+      <MyTextBox/>
       </header>
     </div>
   );
@@ -35,21 +38,19 @@ function App() {
 
 function MyButton({count, onClick}) {
   return (
-    <button onClick={onClick}>
+    <button onClick={onClick}
+    style={{width: '150px', height: '50px', fontSize: '10px',margin:'20px auto'}}>
       Clicked {count} times;
     </button>
   );
 }
 
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>
-        Hello there.<br />How do you do?
-      </p>
-    </>
-  );
+function MyTextBox()
+{
+  return(
+    <TextField label="文章を入力" variant='outlined' fullWidth style={{margin: '20px 0', width: '300px', height: ' 50px', margin: '20px auto'}}/>
+  )
 }
 
 export default App;
+
