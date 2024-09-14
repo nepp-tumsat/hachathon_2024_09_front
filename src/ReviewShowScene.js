a75225b07e10d1cdbce1bf6dcf6329435b1f5d5f
minorscarf <minorscarf@gmail.com>
2024-09-14 23:01:45 +0900

    apiをたたきたい

--- src/ReviewShowScene.js
+++ src/ReviewShowScene.js
@@ -1,6 +1,23 @@
 import './/ReviewShowScene.css';
 
 function App() {
+
+  var info = "";
+  <script>
+  function fetchData(){
+    fetch('http://localhost:8000/get_reviews')
+    .then(response => response.json())
+    .then(data => {
+      const resultDiv = document.getElementById('result');
+      document.write(resultDiv);
+      info = resultDiv;
+    })
+    .catch(error => {
+      console.error('エラー:',error);
+    })
+  }
+  </script>
+
   const posts = Array.from({ length: 100 }, (_, index) => ({
     id: index + 1,
     text: `これは投稿番号 ${index + 1} です。読書祭りのタイムラインにようこそ！`
@@ -10,6 +27,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>祭りだ！祭りだ！読書祭りだ！！！</h1>
+        <h1>info</h1>
       </header>
 
       <main className="body">
